9ece28fe59c124bec0d86efeb32a09f6252fe2a9
Rifat Halim <rhalim@magicleap.com>
2021-12-01 21:21:16 -0500

    Added a new line

--- src/main/java/execute/Execute.java
+++ src/main/java/execute/Execute.java
@@ -3,5 +3,6 @@ package execute;
 public class Execute {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("This is new line");
     }
 }
